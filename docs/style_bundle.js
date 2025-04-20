8aab699a14fb29dd6221dcae827b03c26f68cf7d
ZakharDay <zakhar.day@gmail.com>
2025-04-20 22:15:40 +0300

    Build

--- docs/style_bundle.js
+++ docs/style_bundle.js
@@ -1,5 +1,29 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 
+function init_S_NoticeBar() {
+  var bar = document.querySelector('.S_NoticeBar');
+  if (bar) {
+    var closeButton = bar.querySelector('.S_NoticeBar .A_IconButton');
+    closeButton.addEventListener('click', function () {
+      bar.remove();
+      document.body.classList.remove('notice');
+    });
+  }
+}
+function init_S_SideBar_mobile() {
+  var openButton = document.querySelector('.O_MenuBar .A_IconButton');
+  var closeButton = document.querySelector('.O_SideBarHome .A_IconButton');
+  openButton.addEventListener('click', function () {
+    document.body.classList.add('menu');
+  });
+  closeButton.addEventListener('click', function () {
+    document.body.classList.remove('menu');
+  });
+}
+document.addEventListener('DOMContentLoaded', function () {
+  init_S_NoticeBar();
+  init_S_SideBar_mobile();
+});
 /******/ })()
 ;
